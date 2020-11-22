8f34afd10cfb43a3e3c9eeed738c1b8f27694bf7
Karl 'NaN' Wikström <karl@wikstrom.io>
2020-11-22 10:18:52 +0100

    Add help text for restore cammond in status print

--- internal/status/status.go
+++ internal/status/status.go
@@ -37,6 +37,7 @@ func (s *Status) String() string {
 
 	if len(s.staged) > 0 {
 		fmt.Fprintln(buf, "Changes to be committed:")
+		fmt.Fprintln(buf, "  (use \"git restore --staged <file>...\" to unstage)")
 		for _, staged := range s.staged {
 			fmt.Fprint(buf, color.Green.Sprintf("        %s   %s\n", *staged.changeType, staged.path))
 		}
@@ -45,7 +46,8 @@ func (s *Status) String() string {
 
 	if len(s.unstaged) > 0 {
 		fmt.Fprintln(buf, "Changes not staged for commit:")
-		fmt.Fprintln(buf, "  (use \"git add <file>\" to update what will be committed)")
+		fmt.Fprintln(buf, "  (use \"git add <file>...\" to update what will be committed)")
+		fmt.Fprintln(buf, "  (use \"git restore <file>...\" to discard changes in working directory)")
 		for _, unstaged := range s.unstaged {
 			fmt.Fprint(buf, color.Red.Sprintf("        %s   %s\n", *unstaged.changeType, unstaged.path))
 		}
@@ -54,7 +56,7 @@ func (s *Status) String() string {
 
 	if len(s.untracked) > 0 {
 		fmt.Fprintln(buf, "Untracked files:")
-		fmt.Fprintln(buf, "  (use \"git add <file>\" to include in what will be committed)")
+		fmt.Fprintln(buf, "  (use \"git add <file>...\" to include in what will be committed)")
 		for _, untracked := range s.untracked {
 			fmt.Fprint(buf, color.Red.Sprintf("        %s\n", untracked.path))
 		}
